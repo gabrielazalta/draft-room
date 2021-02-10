bbece9e4d01f61992509e401a51bda9d09d9f9b5
jconeff <jessicaconeff@gmail.com>
2021-02-10 15:43:56 -0500

    fixed new post home page

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -12,49 +12,49 @@ router.get('/', (req, res) => {
     res.render('dashboard')
 });
 
-// // get all posts for dashboard
-// router.get('/', withAuth, (req, res) => {
-//     console.log(req.session);
-//     console.log('======================');
-//     Post.findAll({
-//             where: {
-//                 user_id: req.session.user_id
-//             },
-//             attributes: [
-//                 'id',
-//                 'post_url',
-//                 'title',
-//                 'created_at',
-//                 [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-//             ],
-//             include: [{
-//                     model: Comment,
-//                     attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-//                     include: {
-//                         model: User,
-//                         attributes: ['username']
-//                     }
-//                 },
-//                 {
-//                     model: User,
-//                     attributes: ['username']
-//                 }
-//             ]
-//         })
-//         .then(dbPostData => {
-//             const posts = dbPostData.map(post => post.get({
-//                 plain: true
-//             }));
-//             res.render('dashboard', {
-//                 posts,
-//                 loggedIn: true
-//             });
-//         })
-//         .catch(err => {
-//             console.log(err);
-//             res.status(500).json(err);
-//         });
-// });
+// get all posts for dashboard
+router.get('/', withAuth, (req, res) => {
+    console.log(req.session);
+    console.log('======================');
+    Post.findAll({
+            where: {
+                user_id: req.session.user_id
+            },
+            attributes: [
+                'id',
+                'post_url',
+                'title',
+                'created_at',
+                [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+            ],
+            include: [{
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({
+                plain: true
+            }));
+            res.render('dashboard', {
+                posts,
+                loggedIn: true
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 router.get('/edit/:id', withAuth, (req, res) => {
     Post.findByPk(req.params.id, {
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -28,55 +28,55 @@ router.get('/new-post', (req, res) => {
     res.render('new-post')
 });
 
-// // get all posts for homepage
-// router.get('/', (req, res) => {
-//     console.log('======================');
-//     Post.findAll({
-//             attributes: [
-//                 'id',
-//                 'post_url',
-//                 'title',
-//                 'created_at',
-//                 [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-//             ],
-//             include: [{
-//                     model: Comment,
-//                     attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-//                     include: {
-//                         model: User,
-//                         attributes: ['username']
-//                     }
-//                 },
-//                 {
-//                     model: User,
-//                     attributes: ['username']
-//                 }
-//             ]
-//         })
-//         .then(dbPostData => {
-//             const posts = dbPostData.map(post => post.get({
-//                 plain: true
-//             }));
+// get all posts for homepage
+router.get('/', (req, res) => {
+    console.log('======================');
+    Post.findAll({
+            attributes: [
+                'id',
+                'post_url',
+                'title',
+                'created_at',
+                [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+            ],
+            include: [{
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({
+                plain: true
+            }));
 
-//             res.render('homepage', {
-//                 posts,
-//                 loggedIn: req.session.loggedIn
-//             });
-//         })
-//         .catch(err => {
-//             console.log(err);
-//             res.status(500).json(err);
-//         });
-// });
+            res.render('homepage', {
+                posts,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
-// router.get('/login', (req, res) => {
-//     if (req.session.loggedIn) {
-//         res.redirect('/');
-//         return;
-//     }
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
 
-//     res.render('login');
-// });
+    res.render('login');
+});
 
 router.get('/post/:id', (req, res) => {
     Post.findOne({
