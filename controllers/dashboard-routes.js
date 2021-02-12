9c9f6217bd9b77be29a58a5eababbe0472e7a083
Dalton Morrel <daltonmorrel7&@gmail.com>
2021-02-11 21:11:53 -0500

    merge

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -22,7 +22,7 @@ router.get('/', withAuth, (req, res) => {
             },
             attributes: [
                 'id',
-                'post_url',
+                'content',
                 'title',
                 'created_at',
                 [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
@@ -60,7 +60,7 @@ router.get('/edit/:id', withAuth, (req, res) => {
     Post.findByPk(req.params.id, {
             attributes: [
                 'id',
-                'post_url',
+                'content',
                 'title',
                 'created_at',
                 [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
