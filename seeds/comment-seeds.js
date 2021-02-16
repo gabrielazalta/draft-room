aacc07f55e63f63dbeec01a5180b061e69341d90
Gabriela Zalta <gzalta96@gmail.com>
2021-02-16 11:28:43 -0500

    font changed

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -1,69 +1,69 @@
-const { Comment } = require('../models');
+const {
+  Comment
+} = require('../models');
 
-const commentdata = [
-  {
-    comment_text: 'Nunc rhoncus dui vel sem.',
+const commentdata = [{
+    comment_text: 'cool post bro!',
     user_id: 6,
     post_id: 1
   },
   {
-    comment_text: 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.',
+    comment_text: 'wow, I am going to check that out',
     user_id: 2,
     post_id: 1
   },
   {
-    comment_text: 'Aliquam erat volutpat. In congue.',
+    comment_text: 'hey, I like your posts!',
     user_id: 3,
     post_id: 1
   },
   {
-    comment_text: 'Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.',
+    comment_text: 'nice :)',
     user_id: 3,
     post_id: 2
   },
   {
-    comment_text: 'In hac habitasse platea dictumst.',
+    comment_text: 'love this',
     user_id: 1,
     post_id: 2
   },
   {
-    comment_text: 'Vivamus vestibulum sagittis sapien.',
+    comment_text: 'I agree!',
     user_id: 1,
     post_id: 3
   },
   {
-    comment_text: 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.',
+    comment_text: 'yesss',
     user_id: 3,
     post_id: 3
   },
   {
-    comment_text: 'Sed vel enim sit amet nunc viverra dapibus.',
+    comment_text: 'woohoo!',
     user_id: 2,
     post_id: 2
   },
   {
-    comment_text: 'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat.',
+    comment_text: 'no way!',
     user_id: 3,
     post_id: 1
   },
   {
-    comment_text: 'Morbi a ipsum.',
+    comment_text: 'cool',
     user_id: 3,
     post_id: 2
   },
   {
-    comment_text: 'Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.',
+    comment_text: 'sounds awesome',
     user_id: 3,
     post_id: 1
   },
   {
-    comment_text: 'Donec ut mauris eget massa tempor convallis.',
+    comment_text: 'wow wow',
     user_id: 3,
     post_id: 2
   },
   {
-    comment_text:
-      'Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo.',
+    comment_text: '!!!!!',
     user_id: 1,
     post_id: 1
   }
